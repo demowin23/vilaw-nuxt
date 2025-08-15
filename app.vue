b04e609355331b771c8e981a5f8bdf71ad9f574a
demowin23 <wd6243813@gmail.com>
2025-08-15 15:51:14 +0700

    update

--- app.vue
+++ app.vue
@@ -2,6 +2,15 @@
   <div
     class="bg-gray-100 dark:bg-gray-900 min-h-screen transition-colors duration-300"
   >
+    <noscript
+      ><iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-NVJ6L34C"
+        height="0"
+        width="0"
+        style="display: none; visibility: hidden"
+      ></iframe
+    ></noscript>
+
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
@@ -23,6 +32,16 @@ useHead({
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
         gtag('config', 'G-E67PMGVVTG');
+      `,
+    },
+    // Google Tag Manager setup
+    {
+      innerHTML: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NVJ6L34C');
       `,
     },
   ],
